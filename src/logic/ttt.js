e89bdfc4a6132747ee9cd50e15cf3c00c54e29b9
Ingi <ingi17@ru.is>
2018-10-26 14:31:48 +0000

    CheckWinner og CheckTie virkni og test case

--- src/logic/ttt.js
+++ src/logic/ttt.js
@@ -1,10 +1,75 @@
 var grid,
 	xPoints, 
-	oPoints;
+	oPoints,
+	player,
+	counter,
+	playing;
 
 TTT = function() {
-	grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+	grid = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 	xPoints = 0;
 	oPoints = 0;
+	player = 'X';
+	counter = 0;
+	playing = true;
 	return true;
+}
+
+changeTurn = function() {
+	if (player == 'X') {
+		player = 'O';
+	} else {
+		player = 'X';
+	
+	}	return player;
+}
+
+checkPlayer = function() {
+	return player;
+}
+
+setField = function(field) {
+	if(checkField(field)){
+		grid[field] = checkPlayer();
+		counter++;
+		return grid[field];
+	}
+	return false;
+	
+}
+checkField = function(field){
+	if(grid[field] == field){
+		return true;
+	} else {
+		return false;
+	}
+}
+checkWinner = function(){
+	for(var i = 0; i < 3; i++){
+		if(grid[i] == grid[i+3] && grid[i] == grid[i+6]){
+			playing = false;
+			return player;
+		}	
+	}
+	for(var i = 0; i < 7; i = i+3){
+		if(grid[i] == grid[i+1] && grid[i] == grid[i+2]){
+			playing = false;
+			return player;
+		}	
+	}
+	if(grid[0] == grid[4] && grid[0] == grid[8]) {
+		playing = false;
+        return player;
+
+    } else if(grid[2] == grid[4] && grid[2] == grid[6]){
+    	playing = false;
+        return player;
+    }
+    return false;
+}
+checkTie = function(){
+	if(counter > 8 && playing == true){
+    	return true;
+    } 
+    return false;
 }
--- src/logic/ttt.test.js
+++ src/logic/ttt.test.js
@@ -1,5 +1,27 @@
 const ttt = require("./ttt");
 
 test("Returns true if initiation successful", () => {
-   expect(TTT()).not.toBeNull();
+   expect(TTT()).toBe(true);
+});
+
+test("Returns O twice then X twice.", () => {
+	expect(changeTurn()).toBe('O');
+	expect(checkPlayer()).toBe('O');
+	expect(changeTurn()).toBe('X');
+	expect(checkPlayer()).toBe('X');
+});
+
+test("Returns true, then sets field 0 to X, then returns false cause field is taken, then false when I try to set to field zero again", () => {
+	expect(checkField(0)).toBe(true);
+	expect(setField(0)).toBe('X');
+	expect(checkField(0)).toBe(false);
+	expect(setField(0)).toBe(false);
+});
+test("", () => {
+	expect(TTT()).toBe(true);
+	expect(checkWinner()).toBe(false);
+	expect(setField(0)).toBe('X');
+	expect(setField(1)).toBe('X');
+	expect(setField(2)).toBe('X');
+	expect(checkWinner()).toBe('X');
 });
