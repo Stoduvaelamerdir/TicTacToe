fd0e849d1373c7b0821154196c80a745b938bb9c
arnorerling <arnorerling@gmail.com>
2018-10-26 13:09:59 +0000

    Initiation á ttt

---
+++ src/logic/ttt.js
@@ -0,0 +1,10 @@
+var grid,
+	xPoints, 
+	oPoints;
+
+TTT = function() {
+	grid = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+	xPoints = 0;
+	oPoints = 0;
+	return true;
+}
---
+++ src/logic/ttt.test.js
@@ -0,0 +1,5 @@
+const ttt = require("./ttt");
+
+test("Returns true if initiation successful", () => {
+   expect(TTT()).not.toBeNull();
+});
