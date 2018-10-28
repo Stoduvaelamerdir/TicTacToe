c6fc2a20a90ef30228997f3a761f1f80cf0f7ac1
thorlaugarson <gudmundurht@ru.is>
2018-10-28 18:52:57 +0000

    Fixed tie glitch

--- src/client/index.js
+++ src/client/index.js
@@ -3,117 +3,114 @@ const $ = require("jquery");
 
 var tdArray = document.getElementsByTagName("td");
 
-window.onload = function () {
-    fetch("/api/clearBoard")
-        .catch(error => console.log('Error:', error));
-}
+window.onload = function() {
+  fetch("/api/clearBoard").catch(error => console.log("Error:", error));
+};
 
-$("#replay").click(function () {
-	$(".endgame").css("display", "none");
-    fetch("/api/restartGame")
-        .then(getGame())
-        .catch(error => console.log('Error:', error));
-})
+$("#replay").click(function() {
+  $(".endgame").css("display", "none");
+  fetch("/api/restartGame")
+    .then(getGame())
+    .catch(error => console.log("Error:", error));
+});
 
-$("#reset").click(function () {
-	$(".endgame").css("display", "none");
-    fetch("/api/resetGame")
-        .then(getGame())
-        .catch(error => console.log('Error:', error));
-})
+$("#reset").click(function() {
+  $(".endgame").css("display", "none");
+  fetch("/api/resetGame")
+    .then(getGame())
+    .catch(error => console.log("Error:", error));
+});
 
 for (var i = 0; i < tdArray.length; i++) {
-    (function (index) {
-        tdArray[index].addEventListener("click", function () {
-            checkMove(index);
-        })
-    })(i);
+  (function(index) {
+    tdArray[index].addEventListener("click", function() {
+      checkMove(index);
+    });
+  })(i);
 }
-function updateGame(res){
-	updateTable(res.grid);
-	updatePlayer(res.player);
-	updateScore(res.xScore , res.oScore);
+function updateGame(res) {
+  updateTable(res.grid);
+  updatePlayer(res.player);
+  updateScore(res.xScore, res.oScore);
 }
 
 function updateTable(grid) {
-    for (var i = 0; i < grid.length; i++) {
-        var square = document.getElementById('f' + i);
-        if (i != grid[i]) {
-            square.innerHTML = grid[i];
-        } else {
-        	square.innerHTML = "";
-        }
+  for (var i = 0; i < grid.length; i++) {
+    var square = document.getElementById("f" + i);
+    if (i != grid[i]) {
+      square.innerHTML = grid[i];
+    } else {
+      square.innerHTML = "";
     }
+  }
 }
 function updatePlayer(player) {
-    $("#turns").text(player + " Turn")
+  $("#turns").text(player + " Turn");
 }
-function updateScore(x,o) {
-	console.log(x + o)
-   $("#xP").text("X points "+ x);
-   $("#oP").text("O points "+ o);
+function updateScore(x, o) {
+  console.log(x + o);
+  $("#xP").text("X points " + x);
+  $("#oP").text("O points " + o);
 }
 function addMove(number) {
-    var square = number;
-    fetch("/api/addTurn/" + square)
-        .then(checkWinner())
-        .then(checkTie())
-        .then(changePlayer())
-        .then(getGame())
-        .catch(error => console.log('Error:', error));
-
-};
+  var square = number;
+  fetch("/api/addTurn/" + square)
+    .then(checkTie())
+    .then(checkWinner())
+    .then(changePlayer())
+    .then(getGame())
+    .catch(error => console.log("Error:", error));
+}
 
 function getGame() {
-    fetch("/api/getGame")
-        .then(res => res.json())
-        .then(res => updateGame(res))
-        .catch(error => console.log('Error:', error))
+  fetch("/api/getGame")
+    .then(res => res.json())
+    .then(res => updateGame(res))
+    .catch(error => console.log("Error:", error));
 }
 
 function checkWinner() {
-    fetch("/api/checkWinner")
-        .then(res => res.json())
-        .then(res => promptWinner(res))
-        .catch(error => console.log('Error:', error))
+  fetch("/api/checkWinner")
+    .then(res => res.json())
+    .then(res => promptWinner(res))
+    .catch(error => console.log("Error:", error));
 }
-function checkTie(){
-    fetch("/api/checkTie")
-        .then(res => res.json())
-        .then(res => promptTie(res))
-        .catch(error => console.log('Error:', error))
+function checkTie() {
+  fetch("/api/checkTie")
+    .then(res => res.json())
+    .then(res => promptTie(res))
+    .catch(error => console.log("Error:", error));
 }
-
 
 function promptWinner(res) {
-    if(res.winner == 'X' ||res.winner == 'O'){
-    	$(".endgame").css("display", "block");
-        $("#promter").text(res.winner + " WON!!!");
+  if (res.winner == "X" || res.winner == "O") {
+    $(".endgame").css("display", "block");
+    $("#promter").text(res.winner + " WON!!!");
 
-        endCurrentGame()
-        
-    }
+    endCurrentGame();
+  }
 }
 function promptTie(res) {
-   	if(res.tie == true) {
-   		$(".endgame").css("display", "block");
-        $("#promter").text("TIE!!!");
-        endCurrentGame()
-    }
+  if (res.tie == true) {
+    $(".endgame").css("display", "block");
+    $("#promter").text("TIE!!!");
+    endCurrentGame();
+  }
 }
 function changePlayer() {
-	fetch("/api/changePlayer")
-        .catch(error => console.log('Error:', error));
+  fetch("/api/changePlayer").catch(error => console.log("Error:", error));
 }
 function endCurrentGame() {
-	fetch("/api/endCurr")
-        .catch(error => console.log('Error:', error));
+  fetch("/api/endCurr").catch(error => console.log("Error:", error));
 }
 function checkMove(field) {
-	fetch("/api/checkField/" + field)
-        .then(res => res.json())
-        .then(res =>{ if(res){addMove(field)}})
-        .then()
-        .catch(error => console.log('Error:', error))
-    
+  fetch("/api/checkField/" + field)
+    .then(res => res.json())
+    .then(res => {
+      if (res) {
+        addMove(field);
+      }
+    })
+    .then()
+    .catch(error => console.log("Error:", error));
 }
