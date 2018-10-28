83b04bcb26edd57d08d9dac8a20e2cc1f5a39bdd
Ingi <ingi17@ru.is>
2018-10-28 23:03:30 +0000

    Puppeteer end-to-end tests finally functional (I hope)

--- src/tmp/puppTest.test.js
+++ src/tmp/puppTest.test.js
@@ -5,7 +5,7 @@ describe('TicTacToe game tests', () => {
     let pageURL = 'https://glacial-inlet-92555.herokuapp.com/';
 
     beforeAll(async () => {
-        browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
         page = await browser.newPage();
     });
 
@@ -18,32 +18,25 @@ describe('TicTacToe game tests', () => {
             waitUntil: ['load','domcontentloaded','networkidle0','networkidle2']
         });
         await page.click('#f0');
-        //await page.waitFor('#f0');
-        //await page.waitFor(1000);
-
-        field = await page.evaluate(() => document.querySelector('.title > p').innerHTML);
-        expect(field).toBe('The Tic Tac Toe game!');
+        await page.waitFor(500);
 
         field = await page.evaluate(() => document.querySelector('#f0').innerHTML);
         expect(field).toBe('X');
-        // await page.screenshot( {path: './src/tmp/sc.png', type: 'png'} ); // FOR TESTING
+        //await page.screenshot( {path: './src/tmp/sc.png', type: 'png'} ); // FOR TESTING
     });
 
     test('X in field 1, O in field 4', async () => {
-        await page.goto(pageURL, {
-             waitUntil: ['load','domcontentloaded','networkidle0','networkidle2']
+        await page.goto(pageURL, { 
+            waitUntil: ['load','domcontentloaded','networkidle0','networkidle2']
         });
         await page.click('#f0');
-        //await page.waitFor(1000);
-    
-        field = await page.evaluate(() => document.querySelector('#f0').innerHTML);
-
-        expect(field).toBe('X');
-
+        await page.waitFor(500);
         await page.click('#f4');
+        await page.waitFor(500);
 
+        field = await page.evaluate(() => document.querySelector('#f0').innerHTML);
+        expect(field).toBe('X');
         field = await page.evaluate(() => document.querySelector('#f4').innerHTML);
-
         expect(field).toBe('O');
         //await page.screenshot( {path: './src/tmp/sc2.png', type: 'png'} ); // FOR TESTING
     });
@@ -52,14 +45,20 @@ describe('TicTacToe game tests', () => {
         await page.goto(pageURL, { 
             waitUntil: ['load','domcontentloaded','networkidle0','networkidle2']
         });
+
+        await page.click('#reset');
         
         await page.click('#f0'); // x
+        await page.waitFor(500);
         await page.click('#f4'); // o
-        //await page.waitFor(1000);
+        await page.waitFor(500);
         await page.click('#f1'); // x
+        await page.waitFor(500);
         await page.click('#f3'); // o
+        await page.waitFor(500);
         await page.click('#f2'); // x
-        //await page.waitFor(1000);
+        await page.waitFor(500);
+        await page.waitForSelector('#f2');
 
         //await page.screenshot( {path: './src/tmp/sc3.png', type: 'png'} ); // FOR TESTING
     
@@ -79,8 +78,8 @@ describe('TicTacToe game tests', () => {
         expect(field).toBe('X');
 
         // X should have won by now, checking endgame field for text including X (e.g. Winner: X)
-        field = await page.evaluate(() => document.querySelector('#endgame').innerHTML);
-        expect(field).toBe('X');
+        field = await page.evaluate(() => document.querySelector('#promter').innerHTML);
+        expect(field).toBe('X WON!!!');
 
         // Game should have ended, extra clicks shouldnt fill a field
         await page.click('#f8');
