066e118e3c8661672b0a2b68f64889c6e6413f98
arnorerling <arnorerling@gmail.com>
2018-10-28 21:37:30 +0000

    restart, reset, finishGame föll bætt við

--- src/logic/ttt.js
+++ src/logic/ttt.js
@@ -76,25 +76,30 @@ checkWinner = function(){
 		if(grid[i] == grid[i+3] && grid[i] == grid[i+6]){
 			addScore(player);
 			winner = player;
+			finishGame();
 		}	
 	}
 	for(var i = 0; i < 7; i = i+3){
 		if(grid[i] == grid[i+1] && grid[i] == grid[i+2]){
 			addScore(player);
 			winner = player;
+			finishGame();
 		}	
 	}
 	if(grid[0] == grid[4] && grid[0] == grid[8]) {
 		addScore(player);
         winner = player;
+        finishGame();
 
     } else if(grid[2] == grid[4] && grid[2] == grid[6]){
     	addScore(player);
         winner = player;
+        finishGame();
 
     }
     else if(counter > 8 && playing == true){
     	winner = "Tie"
+    	finishGame();
     } 
 }
 
@@ -119,6 +124,27 @@ hardReset = function(){
 		counter = 0;
 		playing = true;
 		winner = false;
+}
+
+restartGame = function(){
+	if(!playing){
+		grid = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+		player = 'X';
+		counter = 0;
+		playing = true;
+		winner = false;
+	}
+}
+
+resetGame = function(){
+	if(!playing){
+		hardReset();
+	}
+}
+finishGame = function() {
+	if(playing){
+		playing = false;
+	}
 }
 
 module.export = TTT();
