5cb7a81a16e98a09fc0744d2a64646bf21a42473
maggi321 <magsterinn@gmail.com>
2018-10-28 23:24:47 +0000

    new test added for new code

--- src/server/api.js
+++ src/server/api.js
@@ -10,17 +10,13 @@ router.get("/move/:square", (req, res) => {
   res.status(200).send(setField(req.params.square));
 });
 
-router.get("/clearBoard", (req, res) => {
-  res.status(200).send(hardReset())
-})
-
 router.get("/resetGame", (req, res) => {
-  res.status(200).send(hardReset())
-})
+  res.status(200).send(hardReset());
+});
 
 router.get("/restartGame", (req, res) => {
-  res.status(200).send(restartGame())
-})
+  res.status(200).send(restartGame());
+});
 
 
 module.exports = router;
